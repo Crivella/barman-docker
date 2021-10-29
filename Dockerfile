fc526e6c15d6c4b330fc378274817c3496fc54ed
Crivella <davidecrivella@yahoo.it>
2021-10-29 17:57:53 +0200

    Added prometheus-node-exporter for use with grapahna-dashboard. Split HOME from DATA_DIR. Properly running the crontab. Imporved several checks for container restart

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM debian:buster
 
 # Install gosu
-ENV GOSU_VERSION=1.11
+# ENV GOSU_VERSION=1.11
 
 RUN apt-get update \
 	&& apt-get install -y --no-install-recommends ca-certificates wget gnupg2 gosu\
@@ -41,6 +41,7 @@ RUN bash -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ buster-pgdg main
 		rsync \
         gettext-base \
         procps \
+        prometheus-node-exporter \
 	&& rm -rf /var/lib/apt/lists/* \
 	&& rm -f /etc/crontab /etc/cron.*/* \
 	&& sed -i 's/\(.*pam_loginuid.so\)/#\1/' /etc/pam.d/cron \
@@ -49,18 +50,24 @@ RUN bash -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ buster-pgdg main
 # Set up some defaults for file/directory locations used in entrypoint.sh.
 ENV \
 	BARMAN_VERSION=2.15 \
-	BARMAN_CRON_SRC=/private/cron.d \
-	BARMAN_DATA_DIR=/var/lib/barman \
+	BARMAN_HOME_DIR=/var/lib/barman \
+	BARMAN_DATA_DIR=/barman_data \
 	BARMAN_LOG_DIR=/var/log/barman \
-	BARMAN_SSH_KEY_DIR=/private/ssh \
-	BARMAN_PGPASSFILE=/private/pgpass \
     BARMAN_CRON_SCHEDULE="* * * * *" \
     BARMAN_BACKUP_SCHEDULE="0 4 * * *" \
     BARMAN_LOG_LEVEL=INFO \
     BARMAN_BACKUP_OPTIONS="concurrent_backup" \
     BARMAN_RETENTION_POLICY="RECOVERY WINDOW of 3 MONTHS" \
-    IMMEDIATE_FIRST_BACKUP="no"
+    IMMEDIATE_FIRST_BACKUP="yes" \
+    BARMAN_EXPORTER_LISTEN_ADDRESS="0.0.0.0" \
+    BARMAN_EXPORTER_LISTEN_PORT=9780 \
+    BARMAN_EXPORTER_CACHE_TIME=120 \
+    NODE_EXPORTER_LISTEN_ADDRESS="0.0.0.0" \
+    NODE_EXPORTER_LISTEN_PORT=9781 
     # BARMAN_RECOVERY_OPTIONS = "" \
+	# BARMAN_CRON_SRC=/private/cron.d \
+	# BARMAN_SSH_KEY_DIR=/private/ssh \
+	# BARMAN_PGPASSFILE=/private/pgpass \
     # DB_HOST=pg \
     # DB_PORT=5432 \
     # DB_SUPERUSER=postgres \
@@ -71,20 +78,21 @@ ENV \
     # DB_SLOT_NAME=barman \
     # DB_BACKUP_METHOD=postgres
     # BARMAN_EXPORTER_SCHEDULE="*/5 * * * *" \
-    # BARMAN_EXPORTER_LISTEN_ADDRESS="0.0.0.0" \
-    # BARMAN_EXPORTER_LISTEN_PORT=9780 \
-    # BARMAN_EXPORTER_CACHE_TIME=3600
+VOLUME ${BARMAN_HOME_DIR}
 VOLUME ${BARMAN_DATA_DIR}
+VOLUME /etc/barman.d
+
+EXPOSE 9780/tcp
+EXPOSE 9781/tcp
 
 COPY install_barman.sh /tmp/
 RUN /tmp/install_barman.sh && rm /tmp/install_barman.sh
 COPY barman.conf.template /etc/barman.conf.template
 # COPY pg.conf.template /etc/barman.d/pg.conf.template
-COPY wal_archiver.py /usr/local/lib/python3.7/dist-packages/barman/wal_archiver.py
+# COPY wal_archiver.py /usr/local/lib/python3.7/dist-packages/barman/wal_archiver.py
 
 # Install barman exporter
-# RUN pip install barman-exporter && mkdir /node_exporter
-# VOLUME /node_exporter
+RUN pip install barman-exporter
 
 # ENV TINI_VERSION v0.18.0
 # ADD https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini /tini
@@ -93,9 +101,9 @@ COPY wal_archiver.py /usr/local/lib/python3.7/dist-packages/barman/wal_archiver.
 #  && gpg --verify /tini.asc \
 #  && chmod +x /tini
 
-CMD ["cron", "-L", "4",  "-f"]
+CMD tail -f ${BARMAN_LOG_DIR}/barman.log
 COPY entrypoint.sh /
-WORKDIR ${BARMAN_DATA_DIR}
+WORKDIR ${BARMAN_HOME_DIR}
 
 # Install the entrypoint script.  It will set up ssh-related things and then run
 # the CMD which, by default, starts cron.  The 'barman -q cron' job will get
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,25 +3,27 @@
 echo "Starting SSH server"
 /etc/init.d/ssh start
 
-echo "Setting ownership/permissions on ${BARMAN_DATA_DIR} and ${BARMAN_LOG_DIR}"
+echo "Setting ownership/permissions on ${BARMAN_DATA_DIR} and ${BARMAN_HOME_DIR} and ${BARMAN_LOG_DIR}"
 
 install -d -m 0700 -o barman -g barman ${BARMAN_DATA_DIR}
+install -d -m 0700 -o barman -g barman ${BARMAN_HOME_DIR}
 install -d -m 0755 -o barman -g barman ${BARMAN_LOG_DIR}
+chown -R barman:barman ${BARMAN_HOME_DIR}
 
 echo "Generating Barman configurations"
 cat /etc/barman.conf.template | envsubst > /etc/barman.conf
 
-if [[ -d ${BARMAN_DATA_DIR}/.pgtemplates ]]; then
-    for f in `ls ${BARMAN_DATA_DIR}/.pgtemplates`; do
-        cat ${BARMAN_DATA_DIR}/.pgtemplates/${f} | envsubst > /etc/barman.d/${f}
-    done
-fi
+# if [[ -d ${BARMAN_DATA_DIR}/.pgtemplates ]]; then
+#     for f in `ls ${BARMAN_DATA_DIR}/.pgtemplates`; do
+#         cat ${BARMAN_DATA_DIR}/.pgtemplates/${f} | envsubst > /etc/barman.d/${f}
+#     done
+# fi
 
-SERVERS=`barman list-servers | cut -d- -f1`
-echo "Identified pg servers: ${SERVERS}"
+SERVERS=`barman list-servers --minimal | tr -s '\n' ' '`
+echo "Servers found: ${SERVERS}"
 echo "Generating cron schedules"
-echo "${BARMAN_CRON_SCHEDULE} barman for H in $SERVERS; do /usr/local/bin/barman receive-wal --create-slot ${H}; done; /usr/local/bin/barman cron" >> /etc/cron.d/barman
-echo "${BARMAN_BACKUP_SCHEDULE} barman cron && barman /usr/local/bin/barman backup all" >> /etc/cron.d/barman
+echo "${BARMAN_CRON_SCHEDULE} barman /bin/bash -c 'for H in $SERVERS; do /usr/local/bin/barman receive-wal --create-slot \${H}; done'; /usr/local/bin/barman cron" > /etc/cron.d/barman
+echo "${BARMAN_BACKUP_SCHEDULE} barman /usr/local/bin/barman cron && barman /usr/local/bin/barman backup all" >> /etc/cron.d/barman
 
 # cat /etc/barman.d/pg.conf.template | envsubst > /etc/barman.d/${DB_HOST}.conf
 # if [[ "${BARMAN_RECOVERY_OPTIONS}" != "" ]]; then
@@ -29,16 +31,15 @@ echo "${BARMAN_BACKUP_SCHEDULE} barman cron && barman /usr/local/bin/barman back
 # fi
 # echo "${DB_HOST}:${DB_PORT}:*:${DB_SUPERUSER}:${DB_SUPERUSER_PASSWORD}" > ${BARMAN_DATA_DIR}/.pgpass
 # echo "${DB_HOST}:${DB_PORT}:*:${DB_REPLICATION_USER}:${DB_REPLICATION_PASSWORD}" >> ${BARMAN_DATA_DIR}/.pgpass
-if [[ -f ${BARMAN_DATA_DIR}/.pgpass ]]; then
-    chown barman:barman ${BARMAN_DATA_DIR}/.pgpass
-    chmod 600 ${BARMAN_DATA_DIR}/.pgpass
+if [[ -f ${BARMAN_HOME_DIR}/.pgpass ]]; then
+    chmod 600 ${BARMAN_HOME_DIR}/.pgpass
 fi
 
-if [[ -d ${BARMAN_DATA_DIR}/.ssh/ ]]; then
+if [[ -d ${BARMAN_HOME_DIR}/.ssh/ ]]; then
     echo "Setting up Barman SSH dir"
     chmod 700 ~barman/.ssh
     chown barman:barman -R ~barman/.ssh
-    if [[ -f ${BARMAN_DATA_DIR}/.ssh/id_rsa ]]; then
+    if [[ -f ${BARMAN_HOME_DIR}/.ssh/id_rsa ]]; then
         echo "Setting up Barman private key"
         chmod 600 ~barman/.ssh/id_rsa
     fi
@@ -48,23 +49,36 @@ echo "Checking/Creating replication slot"
 for H in $SERVERS; do
     barman replication-status ${H} --minimal --target=wal-streamer | grep barman || barman receive-wal --create-slot ${H}
     barman replication-status ${H} --minimal --target=wal-streamer | grep barman || barman receive-wal --reset ${H}
-    # Has to be run before barman can work properly in `streaming only` mode`
-    # sleep 10
 
-    barman cron && barman switch-wal --force --archive --archive-timeout 120 ${H}
+    barman cron
+    # Has to be run before barman can work properly in `streaming only` mode`
+    if [[ `barman check $H | grep "WAL archive: FAILED"` != "" ]]; then
+        barman switch-wal --force --archive --archive-timeout 120 ${H}
+    fi
 done
 
+# run barman exporter every BARMAN_EXPORTER_CACHE_TIME 
+exec /usr/local/bin/barman-exporter -l ${BARMAN_EXPORTER_LISTEN_ADDRESS}:${BARMAN_EXPORTER_LISTEN_PORT} -c ${BARMAN_EXPORTER_CACHE_TIME} &
+echo "Started Barman exporter on ${BARMAN_EXPORTER_LISTEN_ADDRESS}:${BARMAN_EXPORTER_LISTEN_PORT}"
+
+# run prometheus-node-exporter for use with the barman grafana dashboard
+prometheus-node-exporter --web.listen-address="${NODE_EXPORTER_LISTEN_ADDRESS}:${NODE_EXPORTER_LISTEN_PORT}" &
+echo "Started prometheus-node-exporter on ${NODE_EXPORTER_LISTEN_ADDRESS}:${NODE_EXPORTER_LISTEN_PORT}"
 
 echo "Initializing done"
 
 if [[ ${IMMEDIATE_FIRST_BACKUP} == "yes" ]]; then
-    echo "Running first backup..."
-    barman cron && barman backup --wait all
-    echo "...done"
+    for H in $SERVERS; do
+        if [[ "`barman list-backups $H`" == "" ]]; then
+            echo "Running first backup for server ${H}..."
+            barman cron && barman backup --wait ${H}
+            echo "...done"
+        fi
+    done
 fi
 
-# run barman exporter every hour
-exec /usr/local/bin/barman-exporter -l ${BARMAN_EXPORTER_LISTEN_ADDRESS}:${BARMAN_EXPORTER_LISTEN_PORT} -c ${BARMAN_EXPORTER_CACHE_TIME} &
-echo "Started Barman exporter on ${BARMAN_EXPORTER_LISTEN_ADDRESS}:${BARMAN_EXPORTER_LISTEN_PORT}"
 
-tail -f ${BARMAN_LOG_DIR}/barman.log
+echo "Starting cron service..."
+cron -L 4
+
+exec "$@"
